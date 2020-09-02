5974e8deae8cc3c59a58a71acf94137ee6b4f100
fgasdia <fgasdia@noreply.github.com>
2020-09-02 10:53:05 -0600

    Rearrange files

--- src/PropagationModelPrep.jl
+++ src/PropagationModelPrep.jl
@@ -5,4 +5,21 @@ using DSP
 using LongwaveModeSolver
 const LWMS = LongwaveModeSolver
 
+abstract type ComputeJob end
+
+"""
+walltime "00:57:00" is 57 minutes
+"""
+struct Summit <: ComputeJob
+    runname::String
+    rundir::String
+    numnodes::Int
+    walltime::String
+    exefile::String
+end
+
+include("utils.jl")
+include("MSISatmosphere.jl")
+include("emp2d.jl")
+
 end  # module
--- src/emp2d.jl
+++ src/emp2d.jl
@@ -1,17 +1,6 @@
-
-
-abstract type ComputeJob end
-
-"""
-walltime "00:57:00" is 57 minutes
-"""
-struct Summit <: ComputeJob
-    runname::String
-    rundir::String
-    numnodes::Int
-    walltime::String
-    exefile::String
-end
+#==
+EMP2D STRUCTS
+==#
 
 struct Defaults
     taur::Float64
@@ -137,10 +126,9 @@ mutable struct Ground
     Ground() = new()
 end
 
-include("utils.jl")
-include("MSISatmosphere.jl")
-
-########
+#==
+FUNCTIONS TO WRITE FILES
+==#
 
 function writeinputs(s::Inputs; path="")
     open(joinpath(path,"inputs.dat"), "w") do f
@@ -213,7 +201,11 @@ function writenu(nu; path="")
     end
 end
 
-function fdtd(file::AbstractString, rundir::String, walltime::String)
+#==
+MAIN FUNCTIONS
+==#
+
+function emp2d(file::AbstractString, rundir::String, walltime::String)
     ispath(file) || error("$file is not a valid file name")
 
     s = LWMS.parse(file)
@@ -301,4 +293,57 @@ function buildandrun(s::LWMS.BasicInput, rundir::String, walltime::String)
     println("Job $jobid submitted!\n")
 
     return nothing
+end
+
+function create_emp_source(def::Defaults, in::Inputs)
+    Iin = zeros(in.tsteps)
+
+    # Current waveform: linear rise, exp decay
+    for t  = 1:in.tsteps
+        if t*in.dt < def.taur
+            Iin[t] = def.I0*t*in.dt/def.taur
+        else
+            Iin[t] = def.I0*exp(-(t*in.dt-def.taur)^2/def.tauf^2) + def.Ic*(1 - exp(-(t*in.dt-def.taur)^2/def.tauf^2))
+        end
+    end
+
+    # Filter waveform
+    fc = def.fcut/(1/in.dt/2);  # cutoff frequency
+    order = 41; # filter order
+
+    window = hamming(order)
+    filter = digitalfilter(Lowpass(fc), FIRWindow(window));
+
+    Iin2 = filt(filter, Iin);
+    Iin2 .= Iin2./maximum(Iin2).*def.I0  # rescaled to peak current
+
+    # Fix delays
+    Iin = [Iin2; zeros(2*round(Int,def.sourcealt/abs(def.rsspeed)/in.dt))]
+
+    Iin[Iin .< 1e-20] .= 1e-20
+
+    # Spatial variation
+    satop = in.nground + floor(Int, def.sourcealt/in.dr1) + 1
+
+    source = permutedims(repeat(Iin, 1, satop))
+
+    return source
+end
+
+function generate_rdr(in::Inputs)
+    rr = convert(Int, in.stepalt/in.dr1 + (in.maxalt - in.stepalt)/in.dr2 + 1)
+
+    r = Vector{Float64}(undef, rr)
+    dr = Vector{Float64}(undef, rr-1)
+
+    r[1] = in.Re
+    for i = 2:rr
+        if r[i-1] < (in.Re + in.stepalt)
+            r[i] = r[i-1] + in.dr1
+        else
+            r[i] = r[i-1] + in.dr2
+        end
+        dr[i-1] = r[i] - r[i-1]
+    end
+    return r, dr
 end
